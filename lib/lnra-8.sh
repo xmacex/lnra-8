103777da0c60332a107579a1fdca609836eef723
Mace Ojala <mace.ojala@gmail.com>
2025-10-05 21:33:01 +0100

    A bit of audio buffering to help with that glitching.

--- lib/lnra-8.sh
+++ lib/lnra-8.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd /home/we/dust/code/lnra-8/lib
 # pd -jack -nojackconnect -nogui -realtime LIRA-8/LIRA-8_Pd_Standalone/_LIRA-8.pd & # Why does the upstream not work directyl, did I add something to it eg. a loadbang?
-pd -r 48000 -noadc -jack -nojackconnect -nogui -realtime LIRA-8_Pd_Standalone/_LIRA-8.pd &
+pd -r 48000 -noadc -jack -nojackconnect -nogui -realtime -audiobuf 64 LIRA-8_Pd_Standalone/_LIRA-8.pd &
 sleep 2
 jack_connect pure_data:output_1 crone:input_1
 jack_connect pure_data:output_2 crone:input_2
